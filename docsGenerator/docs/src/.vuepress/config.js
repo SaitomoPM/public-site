12bc50b88c167b2603fedd1f16f166de4d0e1960
Stephen <62410139+Bobinstein@users.noreply.github.com>
2023-08-05 19:30:44 -0400

    style: correctly configured Rubic  font

--- docsGenerator/docs/src/.vuepress/config.js
+++ docsGenerator/docs/src/.vuepress/config.js
@@ -37,6 +37,20 @@ module.exports = {
       },
     ],
     ["link", { rel: "stylesheet", href: "/assets/css/theme.css" }],
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap",
+      },
+    ],
   ],
 
   /**
@@ -45,6 +59,9 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
+    fontFamily: {
+      base: ['Dancing Script', 'cursive']
+    },
     searchPlaceholder: "Search",
     repo: "",
     editLinks: false,
